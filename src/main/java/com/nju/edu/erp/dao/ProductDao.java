a463317ec0f5d235d7976bdbe4af6c01afbe6fa8
uncln <2896077327@qq.com>
2022-03-16 23:16:23 +0800

    feature: 商品CRUD

--- src/main/java/com/nju/edu/erp/dao/ProductDao.java
+++ src/main/java/com/nju/edu/erp/dao/ProductDao.java
@@ -5,13 +5,19 @@ import com.nju.edu.erp.model.vo.CreateProductVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @Mapper
 public interface ProductDao {
 
-    int createProduct(CreateProductVO createProductVO);
+    int createProduct(ProductPO productPO);
 
     int updateById(ProductPO productPO);
 
     ProductPO findById(String id);
+
+    List<ProductPO> findAll();
+
+    int deleteById(String id);
 }
