3b33f2f5c4b4d6796f26569a3c637deb63e65eba
sky <443093653@qq.com>
2022-03-16 15:56:05 +0800

    feat: 新增Mapper

---
+++ src/main/java/com/nju/edu/erp/dao/ProductDao.java
@@ -0,0 +1,9 @@
+package com.nju.edu.erp.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface ProductDao {
+}
