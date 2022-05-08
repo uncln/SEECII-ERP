551e929ee5e091234dcc638f307fc871ca917e06
uncln <2896077327@qq.com>
2022-05-08 12:18:43 +0800

    fix: 增加枚举的映射类

--- src/main/java/com/nju/edu/erp/enums/handlers/SysEnumTypeHandler.java
+++ src/main/java/com/nju/edu/erp/enums/handlers/SysEnumTypeHandler.java
@@ -1,6 +1,7 @@
 package com.nju.edu.erp.enums.handlers;
 
 import com.nju.edu.erp.enums.BaseEnum;
+import com.nju.edu.erp.enums.CustomerType;
 import com.nju.edu.erp.enums.sheetState.PurchaseSheetState;
 import com.nju.edu.erp.enums.sheetState.WarehouseInputSheetState;
 import com.nju.edu.erp.enums.sheetState.WarehouseOutputSheetState;
@@ -10,7 +11,7 @@ import org.apache.ibatis.type.MappedTypes;
  * 枚举转换的公共模块
  *
  */
-@MappedTypes(value = {PurchaseSheetState.class, WarehouseInputSheetState.class, WarehouseOutputSheetState.class})
+@MappedTypes(value = {PurchaseSheetState.class, WarehouseInputSheetState.class, WarehouseOutputSheetState.class, CustomerType.class})
 public class SysEnumTypeHandler<E extends Enum<E> & BaseEnum> extends BaseEnumTypeHandler<E> {
     /**
      * 设置配置文件设置的转换类以及枚举类内容，供其他方法更便捷高效的实现
